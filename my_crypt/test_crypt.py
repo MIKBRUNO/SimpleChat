0455add2306bc66f6ccd33d0e8d4c3186dfe1e85
MIKBRUNO <mikdyach@mail.ru>
2021-02-13 15:55:18 +0700

    gybrid ciphering tested; starts include it to chat

--- my_crypt/test_crypt.py
+++ my_crypt/test_crypt.py
@@ -2,21 +2,24 @@
 from Crypto.Cipher import AES
 from secrets import token_bytes
 import rsa
-import bcrypt
 import DataProcessing as dp
 
 message = 'super secret Alice\'s message'
 name = 'Alice'
 
+# Alice gen keys for signature
+a_pk, a_sk = rsa.newkeys(500)
+
 # Bob gen keys
-public_key, private_key = rsa.newkeys(600)
+public_key, private_key = rsa.newkeys(500)
 
 # Alice digital signature
 msg = dp.write_json({'text': message, 'name': name})
-salt = bcrypt.gensalt()
+# salt = bcrypt.gensalt()
 print('encoded msg:', msg.encode())
-msg_hash = bcrypt.hashpw(msg.encode(), salt)
-dig_sign = rsa.encrypt(msg_hash, public_key)
+# msg_hash = bcrypt.hashpw(msg.encode(), salt)
+# dig_sign = rsa.encrypt(msg_hash, a_sk)
+dig_sign = rsa.sign(msg.encode(), a_sk, 'SHA-256')
 
 # Alice message crypt
 symmetric_key = token_bytes(16)
@@ -29,11 +32,11 @@ sym_key_encrypted = rsa.encrypt(symmetric_key, public_key)
 
 # Alice sends
 ALICE_MSG = {'text': encrypted.hex(), 'dig_sign': dig_sign.hex(),
-             'sym_key': sym_key_encrypted.hex(), 'nonce': cipher.nonce.hex()}
+             'sym_key': sym_key_encrypted.hex(), 'nonce': rsa.encrypt(cipher.nonce, public_key).hex()}
 print(ALICE_MSG)
 
 # Bob gets
-del message, name, msg, salt, msg_hash,\
+del message, name, msg,\
     dig_sign, symmetric_key, sym_key_encrypted,\
     cipher, encrypted
 
@@ -48,11 +51,10 @@ sym_key = rsa.decrypt(sym_key, private_key)
 print('symmetric_key', sym_key)
 
 text = bytes.fromhex(text)
-cipher = AES.new(sym_key, AES.MODE_EAX, nonce=bytes.fromhex(nonce))
+cipher = AES.new(sym_key, AES.MODE_EAX, nonce=rsa.decrypt(bytes.fromhex(nonce), private_key))
 text = cipher.decrypt(text)
 print('encoded msg:', text)
 text = text.decode()
 dig_sign = bytes.fromhex(dig_sign)
-dig_sign = rsa.decrypt(dig_sign, private_key)
-corrupted = not bcrypt.checkpw(text.encode(), dig_sign)
+corrupted = not rsa.verify(text.encode(), dig_sign, a_pk)
 print('message {} corrupted'.format('not' if not corrupted else ''))
